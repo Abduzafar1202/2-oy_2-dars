f0de88bb297adefbf59fb6792e1f8d2ba39e193a
Abduzafar1202 <aa908870138@gmail.com>
2024-01-11 14:17:10 +0500

    2-oy_2-dars

---
+++ script.js
@@ -0,0 +1,10 @@
+let elNav = document.querySelector(".nav");
+let elBtn = document.querySelector(".hamburger");
+
+elBtn.addEventListener("click" , () => {
+    elNav.classList.toggle("d-none")
+    
+})
+
+
+
